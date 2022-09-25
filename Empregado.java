f92359685e5ebae4e326056b18c7418c21199d24
Julio Fujie <juliofujie.6121@aluno.saojudas.br>
2022-09-25 18:26:20 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,44 @@
+public class Empregado{
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private String nome;
+    private int idade;
+
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+
+    public int getTipo (){
+        return tipo;
+    }
+
+    public double calculaSalario(){
+        if (tipo == 1)
+        return salario;
+        else if (tipo == 2)
+        return salario + salario * comissao;
+        else if (tipo == 3)
+        return salario + bonus;
+        else
+            return 0;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setidade(int idade){
+        this.idade = idade;
+    }
+
+}
